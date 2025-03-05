060342660886abfa28aba7459b17f32a5b4ae2f5
Ahn Ming Loke <lokeahnming@gmail.com>
2025-03-05 06:48:12 -0500

    feat: decouple GetRequestID, GetUserID, GetDomainFromRequest into separate httputils for use in endpoints

--- backend/internal/shared/httputils/httputils_request.go
+++ backend/internal/shared/httputils/httputils_request.go
@@ -1,10 +1,44 @@
 package httputils
 
+import (
+	"net/http"
+
+	authMiddleware "github.com/lokeam/qko-beta/server/middleware"
+)
+
 const (
 	ClientIDHeader = "Client-ID"
 	AuthorizationHeader = "Authorization"
 	BearerPrefix = "Bearer "
 	ContentTypeHeader = "Content-Type"
 	ContentTypeText = "text/plain"
-	XRequestIDHeader = "X-Request-Id"
+	XRequestIDHeader = "X-Request-ID"
+	RequestIDKey contextKey = "requestID"
 )
+
+type contextKey string
+
+func GetRequestID(r *http.Request) string {
+	if id, ok := r.Context().Value(authMiddleware.UserIDKey).(string); ok {
+		return id
+	}
+
+	return r.Header.Get(XRequestIDHeader)
+}
+
+func GetUserID(r *http.Request) string {
+	if id, ok := r.Context().Value(authMiddleware.UserIDKey).(string); ok {
+		return id
+	}
+
+	return ""
+}
+
+func GetDomainFromRequest(r *http.Request, defaultDomain string) string {
+	domain := r.URL.Query().Get("domain")
+	if domain == "" {
+		return defaultDomain
+	}
+
+	return domain
+}
