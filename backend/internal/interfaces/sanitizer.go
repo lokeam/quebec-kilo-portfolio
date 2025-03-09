db61d7ff9cf569ac1404865847118d769e2ddcdc
Ahn Ming Loke <lokeahnming@gmail.com>
2025-03-09 05:30:49 -0400

    fix: add separate interface + method for string sanitization

--- backend/internal/interfaces/sanitizer.go
+++ backend/internal/interfaces/sanitizer.go
@@ -2,4 +2,5 @@ package interfaces
 
 type Sanitizer interface {
 	SanitizeSearchQuery(query string) (string, error)
+	SanitizeString(input string) (string, error)
 }
--- backend/internal/testutils/mocks/mock_sanitizer.go
+++ backend/internal/testutils/mocks/mock_sanitizer.go
@@ -1,28 +1,6 @@
-// internal/testutils/mocks/mock_sanitizer.go
 package mocks
 
-import (
-	"errors"
-
-	"github.com/lokeam/qko-beta/internal/interfaces"
-)
-
-// SimpleMockSanitizer is a mock for interfaces.Sanitizer.
-   type SimpleMockSanitizer struct {
-       // Impl is the overridable function for sanitization.
-       Impl func(query string) (string, error)
-   }
-
-   // SanitizeSearchQuery calls the overridden implementation if set.
-   func (ms *SimpleMockSanitizer) SanitizeSearchQuery(query string) (string, error) {
-       if ms.Impl != nil {
-           return ms.Impl(query)
-       }
-       if query == "<script>alert('trigger xss sanitizer error');</script>" {
-           return "", errors.New("sanitizer failure")
-       }
-       return query, nil
-   }
-
-   // Ensure SimpleMockSanitizer implements interfaces.Sanitizer:
-   var _ interfaces.Sanitizer = (*SimpleMockSanitizer)(nil)
+// NOTE: Method to MockSanitizer (different from SimpleMockSanitizer)
+func (ms *MockSanitizer) SanitizeString(s string) (string, error) {
+    return ms.SanitizeFunc(s)
+}
