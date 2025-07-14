db9033d8bc8ce18439d96ecce36eb1044622f593
Ahn Ming Loke <lokeahnming@gmail.com>
2025-07-14 15:11:14 -0400

    refactor(frontend-error): create error page templating system

---
+++ frontend/src/core/error/pages/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ErrorPage } from '@/core/error/components/ErrorPage';
+
+export default function ErrorPageComponent() {
+  return (
+    <ErrorPage
+      variant="500"
+      title="Something went wrong"
+      subtext="An unexpected error occurred. Please try again."
+      buttonText="Go Home"
+      onButtonClick={() => window.location.href = '/'}
+      role="alert"
+      ariaLive="assertive"
+    />
+  );
+}
